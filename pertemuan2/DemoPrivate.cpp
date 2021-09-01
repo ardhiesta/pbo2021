cc54492e08012d3b8046f898027db9b14c6ced14
ardhiesta <ardhi.mail@gmail.com>
2021-09-01 08:05:24 +0700

    contoh setter & getter

--- pertemuan2/DemoPrivate.cpp
+++ pertemuan2/DemoPrivate.cpp
@@ -4,22 +4,38 @@ using namespace std;
 
 class DemoPrivate {
 // num tidak bisa diakses dari luar blok class DemoPrivate
+private:
     int num;
-public :
+
+// getter dan setter
+// setter : memberi nilai ke variabel
+// getter : mengambil nilai variabel
+
+public:
+    // setter
+    void setNum(int num){
+        DemoPrivate::num = num;
+    }
+
     void fun() {
         num = 20;
     }
 
-    int getnum() {
+    // getter
+    int getNum() {
         return num;
     }
 };
 
 int main() {
     DemoPrivate d;
-    d.fun( );
+    /*d.num = 100;
+    cout << "nilai num: " << d.num << endl;*/
+
+    // panggil setter
+    //d.fun();
+    d.setNum(1450);
     //d.num tidak bisa digunakan karena akses ke variabel tidak bisa dilakukan
-    //d.num = 0;
-    cout<<"num is "<<d.getnum( )<<endl;
+    cout<<"nilai num: "<<d.getNum()<<endl;
     return 0;
 }
